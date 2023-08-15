89b87b72c6f9cd24a32b29f28ad00e3aff4cf22e
angeldollface <a11810349@unet.univie.ac.at>
2023-08-15 16:07:52 +0200

    The fluid way.

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,10 @@ Licensed under the MIT license.
 // Prevents a console window opening on Windows in release.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-/// Importing the "Manager"
-/// trait from Tauri.
-use tauri::Manager;
-
 /// Main point of entry for the Rust
 /// compiler.
 fn main() {
-    tauri::Builder::default().setup(
-        |app| {
-            #[cfg(debug_assertions)] // only include this code on debug builds
-    {
-      let window = app.get_window("main").unwrap();
-      window.open_devtools();
-      window.close_devtools();
-    }
-            Ok(())
-        }
-    )
+    tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("Error while running tauri application");
 }
--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,8 @@ Licensed under the MIT license.
 // Calling Tauri's function handler.
 const { listen } = window.__TAURI__.event;
 
+const { convertFileSrc } = window.__TAURI__.tauri;
+
 let imageEl;
 
 // Updates the UI depending
@@ -14,14 +16,14 @@ window.addEventListener(
   "DOMContentLoaded", 
   () => {
     imageEl = document.querySelector("#imageWindow");
+    listen(
+      'tauri://file-drop', 
+      event => {
+        let path = convertFileSrc(event.payload);
+        imageEl.src = path;
+      }
+    );
   }
 );
 
-listen(
-      'tauri://file-drop', 
-      event => {
-        console.log(event.payLoad);
-        imageEl.src = event.payLoad;
-        console.log(imageEl.src);
-      }
-    );
+
